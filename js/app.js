d5cc45ea797fb82a9d13f025f1fd3c89f131fe2e
Justin Murray <murrayju@addpcs.com>
2014-01-26 17:58:55 -0500

    More dependencies.

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@ requirejs.config({
 	waitSeconds: 0,
 
 	paths: {
-		jquery: "/bower_components/jQuery/jquery.min",
-		'jquery.bootstrap': "/bower_components/bootstrap/dist/js/bootstrap.min",
-		angular: "/bower_components/angular/angular.min",
-		'angular-animate': "/bower_components/angular-animate/angular-animate.min",
-		'angular-strap': "/bower_components/angular-strap/dist/angular-strap.min",
-		'angular-strap-tpl': "/bower_components/angular-strap/dist/angular-strap.tpl",
-		'angular.ui.router': "/bower_components/angular-ui-router/release/angular-ui-router.min",
-		fastclick: "/bower_components/fastclick/lib/fastclick"
+		jquery: "../bower_components/jQuery/jquery.min",
+		'jquery.bootstrap': "../bower_components/bootstrap/dist/js/bootstrap.min",
+		angular: "../bower_components/angular/angular.min",
+		'angular-animate': "../bower_components/angular-animate/angular-animate.min",
+		'angular-strap': "../bower_components/angular-strap/dist/angular-strap.min",
+		'angular-strap-tpl': "../bower_components/angular-strap/dist/angular-strap.tpl",
+		'angular.ui.router': "../bower_components/angular-ui-router/release/angular-ui-router.min",
+		fastclick: "../bower_components/fastclick/lib/fastclick"
 	},
 
 	shim: {
