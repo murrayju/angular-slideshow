97fb335085a23fce9cec516283147c6eeca7db3e
Justin Murray <justin.murray@mmodal.com>
2014-02-14 17:08:50 -0500

    Flushed out the outline.

--- js/slides.js
+++ js/slides.js
@@ -9,6 +9,15 @@ define([], function () {
         {title: 'Title', url: 'slides/title.html'},
         {title: 'What is it?', url: 'slides/what.html'},
         {title: 'How does it work?', url: 'slides/how.html'},
-        {title: 'A simple example', url: 'slides/helloWorld.html'}
+        {title: 'A simple example', url: 'slides/helloWorld.html'},
+        {title: 'Directives', url: 'slides/directives.html'},
+        {title: 'Controllers', url: 'slides/controllers.html'},
+        {title: 'Services', url: 'slides/services.html'},
+        {title: 'Routing', url: 'slides/routing.html'},
+        {title: 'Promises', url: 'slides/promises.html', children: [
+            {title: '$http', url: 'slides/http.html'}
+        ]},
+        {title: 'Modules', url: 'slides/modules.html'},
+        {title: 'Questions?', url: 'slides/end.html'}
 	];
 });
